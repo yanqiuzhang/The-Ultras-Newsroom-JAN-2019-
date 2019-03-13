0b405ce73f9e197fa06b6b8a2c9a00bd68256551
nevroje <joakim.berglind@gmail.com>
2019-03-13 09:42:44 +0100

    added p PR TEMPLATE

--- config/application.rb
+++ config/application.rb
@@ -21,13 +21,15 @@ module TheUltrasNewsroomJan2019
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
-
-    # Don't generate system test files.
+    config.generators do |generate|
+      generate.helper false
+      generate.assets false
+      generate.view_specs false
+      generate.helper_specs false
+      generate.routing_specs false
+      generate.controller_specs false
+      generate.system_tests false
+    end
     config.generators.system_tests = nil
   end
 end
