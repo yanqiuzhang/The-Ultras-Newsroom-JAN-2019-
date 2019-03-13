4c7b481bd152890a9001c53cbff0844cfd55e072
Per Eriksson <per_hegg@hotmail.com>
2019-03-13 12:00:26 +0100

    generate cucumber, create db and remove extra comments

---
+++ features/support/env.rb
@@ -0,0 +1,15 @@
+require 'cucumber/rails'
+require 'coveralls'
+Coveralls.wear_merged!('rails') 
+
+ World(FactoryBot::Syntax::Methods)
+
+ ActionController::Base.allow_rescue = false
+
+ begin
+  DatabaseCleaner.strategy = :transaction
+rescue NameError
+  raise "You need to add database_cleaner to your Gemfile (in the :test group) if you wish to use it."
+end
+
+ Cucumber::Rails::Database.javascript_strategy = :truncation
