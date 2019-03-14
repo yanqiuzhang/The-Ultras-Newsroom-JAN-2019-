e1dde05cc3baaec27f740b3b1f6a772f6305348e
nevroje <joakim.berglind@gmail.com>
2019-03-14 17:38:38 +0100

    added article links in index added basic steps and feature tests added back button

--- features/step_definitions/assertions_steps.rb
+++ features/step_definitions/assertions_steps.rb
@@ -1,3 +1,7 @@
 Then("I should see {string}") do |content|
     expect(page).to have_content content
 end
+
+Then("I should see the {string} page") do |page|
+    expect(page).to have_content page 
+end
