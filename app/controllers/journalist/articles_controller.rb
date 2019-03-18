936fe25383b90b934ccc82365e4896a0026483fa
Per Eriksson <per_hegg@hotmail.com>
2019-03-18 22:36:26 +0100

    WIP trying to solve update issue

--- app/controllers/journalist/articles_controller.rb
+++ app/controllers/journalist/articles_controller.rb
@@ -4,10 +4,14 @@ class Journalist::ArticlesController < ApplicationController
         @articles = Article.all
     end
 
+    def show
+        @article = Article.find(params[:id])
+    end
+
     def create
-        article = Article.new(article_params)
+        @article = Article.new(article_params)
        
-        if article.save
+        if @article.save
             redirect_to new_journalist_article_path, notice: 'Article was successfully created.'
         else
             redirect_to new_journalist_article_path, alert: 'You have to fill out all the fields'
@@ -18,14 +22,26 @@ class Journalist::ArticlesController < ApplicationController
         @article = Article.find(params[:id])
     end
 
-    def show
+    def update
         @article = Article.find(params[:id])
+       
+        if @article.update(article_params)
+            redirect_to journalist_article_path, notice: 'Article was successfully updated.'
+        else
+            redirect_to @article, notice: 'Article was not updated.'
+        end
     end
 
     def destroy
         @article = Article.find(params[:id])
         @article.destroy
        
-        redirect_to new_journalist_article_path
+        redirect_to new_journalist_article_path, notice: 'Article was successfully deleted.'
     end
+
+    private
+
+    def article_params
+        params.require(:article).permit(:title, :content, :lead)
+    end 
 end
