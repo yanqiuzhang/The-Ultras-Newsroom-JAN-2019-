45098ba4bf782a593d9d95d4e95305fc3c4cb9e6
Per Eriksson <per_hegg@hotmail.com>
2019-03-19 16:23:11 +0100

    delete unnessessary test, fix minor indentation

--- features/step_definitions/assertions_steps.rb
+++ features/step_definitions/assertions_steps.rb
@@ -2,10 +2,6 @@ Then("I should see {string}") do |content|
     expect(page).to have_content content
 end
 
-Then("I should be on the {string} page") do |page|
-    expect(current_path).to eq page 
-end
-
 Then("I should not see {string}") do |content|
     expect(page).to have_no_content content
 end  
