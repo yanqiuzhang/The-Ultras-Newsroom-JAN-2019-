c446d38f644105b3761fad9f738c1b3b0223dc49
olivia <olivia@evolve.se>
2019-03-27 22:39:02 +0100

    add DeviseTokenAuth to models/user.rb, add model specs for User, add role:nil to sessions

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ApplicationRecord
     has_many :articles
     devise :database_authenticatable, :registerable,
            :recoverable, :rememberable, :validatable
+    include DeviseTokenAuth::Concerns::User
     enum role: {visitor: 0, journalist: 1, subscriber: 2, editor: 3}
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,11 +6,21 @@ RSpec.describe User, type: :model do
         it { is_expected.to have_db_column :email }
         it { is_expected.to have_db_column :encrypted_password }
         it { is_expected.to have_db_column :role }
-    end
+        it { is_expected.to have_db_column :provider }
+        it { is_expected.to have_db_column :uid }
+        it { is_expected.to have_db_column :reset_password_token }
+        it { is_expected.to have_db_column :reset_password_sent_at }
+        it { is_expected.to have_db_column :remember_created_at }
+        it { is_expected.to have_db_column :confirmation_token }
+        it { is_expected.to have_db_column :confirmed_at }
+        it { is_expected.to have_db_column :confirmation_sent_at }
+        it { is_expected.to have_db_column :unconfirmed_email }
+        it { is_expected.to have_db_column :nickname }
+        it { is_expected.to have_db_column :image }
+        it { is_expected.to have_db_column :tokens }
+        it { is_expected.to have_db_column :created_at }
+        it { is_expected.to have_db_column :updated_at }
 
-    describe 'Validations' do
-        it { is_expected.to validate_presence_of :email }
-        it { is_expected.to validate_presence_of :password } 
     end
 
     describe 'Associations' do
@@ -20,6 +30,29 @@ RSpec.describe User, type: :model do
     describe 'Factory' do
         it 'should have valid Factory' do
             expect(FactoryBot.create(:user)).to be_valid 
+        end
+    end
+
+    describe 'Validations' do
+        it { is_expected.to validate_presence_of(:email) }
+        it { is_expected.to validate_confirmation_of(:password) }
+    
+        context 'should not have an invalid email address' do
+          emails = ['asdf@ ds.com', '@example.com', 'test me @yahoo.com',
+                    'asdf@example', 'ddd@.d. .d', 'ddd@.d']
+    
+          emails.each do |email|
+            it { is_expected.not_to allow_value(email).for(:email) }
+          end
+        end
+    
+        context 'should have a valid email address' do
+          emails = ['asdf@ds.com', 'hello@example.uk', 'test1234@yahoo.si',
+                    'asdf@example.eu']
+    
+          emails.each do |email|
+            it { is_expected.to allow_value(email).for(:email) }
+          end
         end
     end
 end
