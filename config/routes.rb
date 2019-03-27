44a713b77ea2ee5209993541924c7b9c7b10b550
olivia <olivia@evolve.se>
2019-03-27 21:26:38 +0100

    create api/vi routes

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,9 @@ Rails.application.routes.draw do
 
     namespace :api do
         resources :articles, only: [:index]
+
+        namespace :v1 do
+            mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
+        end
     end
 end
