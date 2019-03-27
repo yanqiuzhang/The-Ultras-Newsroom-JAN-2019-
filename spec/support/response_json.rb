ebb70089872f955748879a9f189e752694d4dca5
olivia <olivia@evolve.se>
2019-03-27 22:45:34 +0100

    remove unnecessary space

--- spec/support/response_json.rb
+++ spec/support/response_json.rb
@@ -1,9 +1,9 @@
 module ResponseJSON
-    def response_json
-      JSON.parse(response.body)
-    end
+  def response_json
+    JSON.parse(response.body)
   end
+end
   
-  RSpec.configure do |config|
-    config.include ResponseJSON
-  end
+RSpec.configure do |config|
+  config.include ResponseJSON
+end
--- spec/support/shoulda_matcher.rb
+++ spec/support/shoulda_matcher.rb
@@ -1,10 +1,10 @@
 Shoulda::Matchers.configure do |config|
-    config.integrate do |with|
-      with.test_framework :rspec
-      with.library :rails
-    end
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
   end
+end
   
-  RSpec.configure do |config|
-    config.include(Shoulda::Matchers::ActiveRecord, type: :model)
-  end
+RSpec.configure do |config|
+  config.include(Shoulda::Matchers::ActiveRecord, type: :model)
+end
