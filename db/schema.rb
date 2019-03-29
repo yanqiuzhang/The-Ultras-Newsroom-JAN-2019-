0bf3a421a65d62b84076a3e9f69d87322bd22cef
vick3d <viktoro0090@gmail.com>
2019-03-29 16:24:52 +0100

    dropped an re-created db

--- db/schema.rb
+++ db/schema.rb
@@ -43,8 +43,8 @@ ActiveRecord::Schema.define(version: 2019_03_22_130206) do
     t.datetime "updated_at", null: false
     t.text "lead"
     t.bigint "user_id"
-    t.boolean "premium", default: false
     t.boolean "approved", default: false
+    t.boolean "premium", default: false
     t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
@@ -81,8 +81,6 @@ ActiveRecord::Schema.define(version: 2019_03_22_130206) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "role"
-    t.bigint "article_id"
-    t.index ["article_id"], name: "index_users_on_article_id"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
@@ -90,5 +88,4 @@ ActiveRecord::Schema.define(version: 2019_03_22_130206) do
   end
 
   add_foreign_key "articles", "users"
-  add_foreign_key "users", "articles"
 end
